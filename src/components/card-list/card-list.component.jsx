e8212c11ba91ea1c1f8c752826942db95de72d9c
Alex Valdez <xvaldez0411@gmail.com>
2023-02-27 21:41:05 -0600

    finished styling -- added google font

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,6 +2,8 @@ import {Component} from 'react';
 
 import './card-list.styles.css';
 
+import Card from '../card/card.component';
+
 class CardList extends Component {
 
     render() {
@@ -11,16 +13,8 @@ class CardList extends Component {
             <div className='card-list'>
                 {
                     monsters.map(monster => {
-                        const {name, email, id} = monster;
                         return(
-                        <div className='card-container' key={id}>
-                            <img 
-                                src={`https://robohash.org/${id}?set=set2&size=180x180`} 
-                                alt={`monster ${name}`}
-                            />
-                            <h2>{name}</h2>
-                            <p>{email}</p>
-                        </div>
+                            <Card monster={monster} />
                     )})
                 }
             </div>
