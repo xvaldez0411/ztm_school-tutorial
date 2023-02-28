134227ea956dfdd7696150f61d5e7a3e36743096
Alex Valdez <xvaldez0411@gmail.com>
2023-02-27 19:51:04 -0600

    first commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,19 @@
+import {Component} from 'react'
+
+
+
+class SearchBox extends Component {
+
+    render(){
+        return (
+            <input 
+                // className='search-box' 
+                type='search' 
+                placeholder= {this.props.placeholder}
+                onChange={this.props.onChangeHandler}
+            />
+        )
+    }
+}
+
+export default SearchBox;
