86d3326844ef4c46b3b7b4100dbe1cbe70fbd689
Alex Valdez <xvaldez0411@gmail.com>
2023-02-28 21:51:58 -0600

    finished changing App.js into functional component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import SearchBox from './components/search-box/search-box.component';
 
     const [searchField, setSearchField] = useState('');
     const [monsters, setMonsters] = useState([]);
+    const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
     useEffect(() => {
       fetch('https://jsonplaceholder.typicode.com/users')
@@ -15,14 +16,19 @@ import SearchBox from './components/search-box/search-box.component';
       .then((users) => setMonsters(users))
     }, []);
 
+    useEffect(() => {
+      const newFilteredMonsters = monsters.filter((monster) => {
+        return monster.name.toLocaleLowerCase().includes(searchField);
+      });
+
+      setFilteredMonsters(newFilteredMonsters);
+    }, [monsters, searchField])
+
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString)
     }
 
-  const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-  });
 
     return (
       <div className="App">
